ae33ba19d8ffc14d29518e549a63a2a2608d491f
protaxY <62854654+protaxY@users.noreply.github.com>
2020-11-04 22:34:37 +0300

    Update child.c

--- child.c
+++ child.c
@@ -1,15 +1,19 @@
-//
-// Created by protaxy on 10/19/20.
-//
 #include <stdio.h>
 #include <unistd.h>
 
-int main(){
+int main(int argc, char* argv[]){
+    FILE *file = fopen(argv[1], "w");
+    printf("%s\n", argv[0]);
+    if (file == NULL){
+        printf("fopen error\n");
+        return -1;
+    }
     float result = 0;
     float currentTerm;
     while (read(fileno(stdin), &currentTerm, sizeof(float)) > 0){
         result += currentTerm;
     }
-    printf("%f", result);
+    fprintf(file, "%f", result);
+    fclose(file);
     return 0;
 }
